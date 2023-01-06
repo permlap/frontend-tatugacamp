f98b26672e8c7a4ca1e9dddcfdfe69558b38f2a9
Permlap <permlapteam@gmail.com>
2023-01-06 23:08:47 +0700

    social media link

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -68,7 +68,10 @@ function Navbar() {
         </Button>
         <li className="mr-2 px-4 py-2 rounded-md bg-[#2C7CD1] flex items-center gap-x-2 text-[20px] ">
           <div>
-            <a className="no-underline text-white" href="fb://page/TaTugaCamp">
+            <a
+              className="no-underline text-white"
+              href="fb://page/107002408742438"
+            >
               <ion-icon name="logo-facebook"></ion-icon>
             </a>
           </div>
