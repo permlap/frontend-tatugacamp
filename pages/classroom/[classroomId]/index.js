ad2ff0fa0de5efe27da5b2cc6e9302aa70c15f37
Permlap <permlapteam@gmail.com>
2023-04-01 22:06:46 +0700

    fix text student number

--- pages/classroom/[classroomId]/index.js
+++ pages/classroom/[classroomId]/index.js
@@ -431,7 +431,7 @@ border-none flex items-center justify-center hover:animate-spin bg-transparent a
                                 </div>
 
                                 <div className="font-Kanit text-xl flex items-center justify-start gap-2">
-                                  <div className=" bg-blue-500 font-semibold text-white w-5 h-5 flex items-center justify-center  rounded-md">
+                                  <div className=" font-semibold text-gray-700 w-5 h-5 flex items-center justify-center  rounded-md">
                                     {student.number}
                                   </div>
                                   {student.firstName}
